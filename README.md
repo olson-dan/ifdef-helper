686d2993d291a90cc2c0327990b0c54ad6fbe2fc
olson-dan <olson-dan@users.noreply.github.com>
2019-04-13 12:21:15 -0700

    Add hover support, update README.

--- README.md
+++ README.md
@@ -2,12 +2,26 @@
 
 VSCode Extension to help show active `#ifdef` statements in C and C++ code.
 
-## Features
+## Planned Features
+
+- :ballot_box_with_check: Display the `#ifdef` statements active for any line of code.
+
+- :ballot_box_with_check: Hover over any `#endif` to see its corresponding `#ifdef`.
+
+- :black_square_button: Jump between `#ifdef` and `#endif` statements.
+
+- :black_square_button: Add an auto-generated comment after an `#endif` statement referencing its corresponding `#ifdef`.
 
 ## Requirements
 
 ## Extension Settings
 
 ## Known Issues
+
+* C block-style comments (`/* */`) are not stripped and may result in incorrect parsing.
+
+* `#ifdef` statements are a terrible means of in-source configuration and should be avoided as much as possible.
+
+* Comments added after `#endif` statements, like all comments, are not validated by the compiler to be correct and thus will often not be the most complete or up-to-date source of information available.
 
 ## Release Notes
--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,56 @@
 import * as vscode from 'vscode';
 
+function trimLine(line: String) {
+	let trimmed = line.trim();
+	if (trimmed.startsWith("#")) {
+		// Strip any C++-style line comments so we have cleaner output.
+		trimmed = trimmed.replace(/\/\/.*/, '').trim();
+	}
+	return trimmed;
+}
+
+const hover: vscode.HoverProvider = {
+	provideHover(document, position, token) {
+		let line_num = position.line;
+		let line = document.lineAt(line_num).text.trim();
+		if (!line.startsWith("#endif")) {
+			return;
+		}
+		let endif_count = 1;
+		let stack: string[] = [];
+		while (line_num >= 0) {
+			line_num = line_num - 1;
+			line = trimLine(document.lineAt(line_num).text);
+			if (line.startsWith("#endif")) {
+				endif_count = endif_count + 1;
+			} else if (endif_count === 1 && (line.startsWith("#else") || line.startsWith("#elif"))) {
+				stack.push(line);
+			} else if (line.startsWith("#if")) {
+				endif_count = endif_count - 1;
+				if (endif_count === 0) {
+					stack.push(line);
+					break;
+				}
+			}
+		}
+		if (stack.length > 0) {
+			const text = stack.reverse().join(" -> ");
+			return new vscode.Hover(text);
+		}
+	}
+}
+
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerTextEditorCommand('ifdef-helper.displayIfdefs', (editor, edit) => {
+
+	let hover1 = vscode.languages.registerHoverProvider('c', hover);
+	let hover2 = vscode.languages.registerHoverProvider('cpp', hover);
+
+	let command1 = vscode.commands.registerTextEditorCommand('ifdef-helper.displayIfdefs', (editor, edit) => {
 		let stack: string[] = [];
 		const doc = editor.document;
 		const line_number = editor.selection.active.line + 1;
 		[...Array(line_number).keys()].forEach((i) => {
-			const line = doc.lineAt(i).text.trim();
+			const line = trimLine(doc.lineAt(i).text);
 			if (line.startsWith("#if")) {
 				stack.push(line);
 			} else if (line.startsWith("#else") || line.startsWith("#elif")) {
@@ -25,7 +69,9 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage(stack.join(" -> "));
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(hover1);
+	context.subscriptions.push(hover2);
+	context.subscriptions.push(command1);
 }
 
 export function deactivate() { }
